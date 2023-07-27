d81016937fde5d6e49c8447e208e055b3297be00
fahad <fahadzama451@gmail.com>
2023-07-27 18:01:36 +0530

    my usepopcorn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// function IMDB() {
+//   const [movieRating, setmovieRating] = useState(0);
+//   return (
+//     <div>
+//       <Starrating color={"red"} setmovieRating={setmovieRating} />
+//       <p>you rated this movie {movieRating} stars</p>
+//     </div>
+//   );
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
